3be83ba9572af7b6c2e635954377359a96bd4544
vanbc18 <82776898+vanbc18@users.noreply.github.com>
2022-08-08 23:53:49 +0200

    Update form.php

--- form.php
+++ form.php
@@ -19,7 +19,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = '	smtp.ionos.fr';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'remy.ramette@ets-ramette.fr';                 // SMTP username
-$mail->Password = '27Juin1964';                           // SMTP password
+$mail->Password = 'secret';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
